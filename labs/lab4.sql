69d545752021b95958632cffc044a3dc3ad6f0cd
Diana Neumann <56086653+DianaNeumann@users.noreply.github.com>
2023-04-25 12:59:37 +0300

    feat: update lab-4

--- labs/lab4.sql
+++ labs/lab4.sql
@@ -21,7 +21,7 @@ where sod.UnitPrice * sod.OrderQty = (
 
 -- 3 Найти такие продукты, которые покупал только один покупатель.
 
--- 1)
+-- a)
 select sod.ProductID
 from Sales.SalesOrderDetail as sod
 where sod.ProductID in (
@@ -31,7 +31,7 @@ where sod.ProductID in (
 	having count(distinct soh.CustomerID) = 1
 )
 
--- 2)
+-- b)
 select p.Name
 from Production.Product as p
 where p.ProductID in (
@@ -68,29 +68,87 @@ WHERE sod.UnitPrice in (
 ORDER BY CustomerID
 
 -- Найти самый дорогой красный товар для каждой подкатегории
-select p.Name, p.ProductSubcategoryID, p.ListPrice
+select p.Name, p.ListPrice
 from Production.Product as p
-where p.ListPrice = (
+where p.Color = 'Red' and p.ListPrice = (
 	select max(p1.ListPrice)
 	from Production.Product as p1
-	where p.ProductSubcategoryID = p1.ProductSubcategoryID and p1.Color = 'Red'
+	where p.ProductId = p1.PRoductID
 )
 
 -- Найти название категории с наибольшим количеством товаров (с подзапросом)
 select pc.Name
-from Production.ProductCategory as pc join
-Production.ProductSubcategory as ps 
-on pc.ProductCategoryID = ps.ProductCategoryID join
-Production.Product as p
-on ps.ProductSubcategoryID = p.ProductSubcategoryID
+from Production.ProductCategory as pc
+join Production.ProductSubcategory as ps
+	on pc.ProductCategoryID = ps.ProductCategoryID
+join Production.Product as p
+	on ps.ProductSubcategoryID = p.ProductSubcategoryID
 group by pc.Name
-having count(p.ProductID) = (
-    select top 1 count(p1.ProductID)
-    from Production.ProductCategory as pc1 join
-    Production.ProductSubcategory as ps1 
-    on pc1.ProductCategoryID = ps1.ProductCategoryID join
-    Production.Product as p1
-    on ps1.ProductSubcategoryID = p1.ProductSubcategoryID
-    group by pc1.Name
-    order by count(p1.ProductID) desc 
+having count(p.ProductID) in (
+	select top 1 count(p1.ProductID)
+	from Production.ProductCategory as pc1
+	join Production.ProductSubcategory as ps1
+		on pc1.ProductCategoryID = ps1.ProductCategoryID
+	join Production.Product as p1
+		on ps1.ProductSubcategoryID = p1.ProductSubcategoryID
+	group by pc1.ProductCategoryID
+	order by count(p1.ProductID) desc
+)
+	
+
+select pc.Name
+from Production.ProductCategory as pc
+where pc.ProductCategoryID = (
+	select top 1 ps.ProductCategoryID
+	from Production.ProductSubCategory as ps
+	join Production.Product as p
+	on p.ProductSubcategoryID = ps.ProductSubcategoryID
+	group by ps.ProductCategoryID
+	order by count(*) desc
+	
+	
+-- Найти название и айдишники продуктов, у которых цвет совпадает с такими товарами цена на которые была меньше 5000
+select p.Name, p.ProductID
+from Production.Product as p
+where p.Color in (
+	select p1.Color
+	from Production.Product as p1
+	where p1.ListPrice < 5000
+)
+
+-- Вывести на экран товары и айдишники, у которых цвет совпадает с цветом самого дорогого товара
+select p.Name, p.ProductID
+from Production.Product as p
+where p.Color in (
+	select top 1 p1.Color
+	from Production.Product as p1
+	group by p1.Color
+	order by max(p1.ListPrice) desc	
+)
+
+-- Найти номера чеков, таких что покупатели, к которым относятся эти чеки, ходили в магазин более трех раз, т.е. имеют более трех чеков
+select soh.SalesOrderID
+from Sales.SalesOrderHeader as soh
+where soh.CustomerID in (
+	select soh1.CustomerID
+	from Sales.SalesOrderHeader as soh1
+	group by soh1.CustomerID
+	having count(soh1.CustomerID) < 3
+)
+
+-- Вывести имена категорий, в которых количество товаров красного цвета меньше товаров черного цвета
+select pc.Name
+from Production.ProductCategory as pc
+where (
+	select count(p1.PRoductID)
+	from Production.Product as p1 
+	join Production.ProductSubcategory as ps1
+		on p1.ProductSubcategoryID = ps1.ProductSubcategoryID
+	where p1.Color = 'Red' and pc.ProductCategoryID = ps1.ProductCategoryID
+) < (
+	select count(p2.PRoductID)
+	from Production.Product as p2
+	join Production.ProductSubcategory as ps2
+		on p2.ProductSubcategoryID = ps2.ProductSubcategoryID
+	where p2.Color = 'Black' and pc.ProductCategoryID = ps2.ProductCategoryID
 )
